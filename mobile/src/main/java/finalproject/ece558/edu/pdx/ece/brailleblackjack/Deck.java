5d5b5734a79d0dd96896a90f8f140ed5c4d69df6
halameer <halameer@users.noreply.github.com>
2015-03-05 01:21:44 -0800

    1. Added card description strings in strings.xml 2. Renamed card braille images dues to requirements of starting a resource with a lowercase letter and not a number

--- mobile/src/main/java/finalproject/ece558/edu/pdx/ece/brailleblackjack/Deck.java
+++ mobile/src/main/java/finalproject/ece558/edu/pdx/ece/brailleblackjack/Deck.java
@@ -25,10 +25,10 @@ public class Deck {
      *  and the card's value as an integer (1-10)
      *  NOTE: Ace will be identified as 1 but it has a possible value of 11
      */
-    public long insertData(String key,
+    public long insertCard(String key,
                            String description,
                            String drawable,
-                           String value){
+                           int value){
         SQLiteDatabase db = storage.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(DeckDatabase.getCardKey(), key);
@@ -88,5 +88,65 @@ public class Deck {
                 cursor.getString(index3),
                 cursor.getInt(index4));
         return card;
+    }
+
+    public  void addCardsToDB(Context context){
+        // Adding club suit
+        insertCard("1_of_clubs", context.getString(R.string.description_1_clubs), null, 1);
+        insertCard("2_of_clubs", context.getString(R.string.description_2_clubs), null, 2);
+        insertCard("3_of_clubs", context.getString(R.string.description_3_clubs), null, 3);
+        insertCard("4_of_clubs", context.getString(R.string.description_4_clubs), null, 4);
+        insertCard("5_of_clubs", context.getString(R.string.description_5_clubs), null, 5);
+        insertCard("6_of_clubs", context.getString(R.string.description_6_clubs), null, 6);
+        insertCard("7_of_clubs", context.getString(R.string.description_7_clubs), null, 7);
+        insertCard("8_of_clubs", context.getString(R.string.description_8_clubs), null, 8);
+        insertCard("9_of_clubs", context.getString(R.string.description_9_clubs), null, 9);
+        insertCard("10_of_clubs", context.getString(R.string.description_10_clubs), null, 10);
+        insertCard("11_of_clubs", context.getString(R.string.description_11_clubs), null, 10);
+        insertCard("12_of_clubs", context.getString(R.string.description_12_clubs), null, 10);
+        insertCard("13_of_clubs", context.getString(R.string.description_13_clubs), null, 10);
+        // Adding diamonds suit
+        insertCard("1_of_diamonds", context.getString(R.string.description_1_diamonds), null, 1);
+        insertCard("2_of_diamonds", context.getString(R.string.description_2_diamonds), null, 2);
+        insertCard("3_of_diamonds", context.getString(R.string.description_3_diamonds), null, 3);
+        insertCard("4_of_diamonds", context.getString(R.string.description_4_diamonds), null, 4);
+        insertCard("5_of_diamonds", context.getString(R.string.description_5_diamonds), null, 5);
+        insertCard("6_of_diamonds", context.getString(R.string.description_6_diamonds), null, 6);
+        insertCard("7_of_diamonds", context.getString(R.string.description_7_diamonds), null, 7);
+        insertCard("8_of_diamonds", context.getString(R.string.description_8_diamonds), null, 8);
+        insertCard("9_of_diamonds", context.getString(R.string.description_9_diamonds), null, 9);
+        insertCard("10_of_diamonds", context.getString(R.string.description_10_diamonds), null, 10);
+        insertCard("11_of_diamonds", context.getString(R.string.description_11_diamonds), null, 10);
+        insertCard("12_of_diamonds", context.getString(R.string.description_12_diamonds), null, 10);
+        insertCard("13_of_diamonds", context.getString(R.string.description_13_diamonds), null, 10);
+        // Adding hearts suit
+        insertCard("1_of_hearts", context.getString(R.string.description_1_hearts), null, 1);
+        insertCard("2_of_hearts", context.getString(R.string.description_2_hearts), null, 2);
+        insertCard("3_of_hearts", context.getString(R.string.description_3_hearts), null, 3);
+        insertCard("4_of_hearts", context.getString(R.string.description_4_hearts), null, 4);
+        insertCard("5_of_hearts", context.getString(R.string.description_5_hearts), null, 5);
+        insertCard("6_of_hearts", context.getString(R.string.description_6_hearts), null, 6);
+        insertCard("7_of_hearts", context.getString(R.string.description_7_hearts), null, 7);
+        insertCard("8_of_hearts", context.getString(R.string.description_8_hearts), null, 8);
+        insertCard("9_of_hearts", context.getString(R.string.description_9_hearts), null, 9);
+        insertCard("10_of_hearts", context.getString(R.string.description_10_hearts), null, 10);
+        insertCard("11_of_hearts", context.getString(R.string.description_11_hearts), null, 10);
+        insertCard("12_of_hearts", context.getString(R.string.description_12_hearts), null, 10);
+        insertCard("13_of_hearts", context.getString(R.string.description_13_hearts), null, 10);
+        // Adding spades suit
+        insertCard("1_of_spades", context.getString(R.string.description_1_spades), null, 1);
+        insertCard("2_of_spades", context.getString(R.string.description_2_spades), null, 2);
+        insertCard("3_of_spades", context.getString(R.string.description_3_spades), null, 3);
+        insertCard("4_of_spades", context.getString(R.string.description_4_spades), null, 4);
+        insertCard("5_of_spades", context.getString(R.string.description_5_spades), null, 5);
+        insertCard("6_of_spades", context.getString(R.string.description_6_spades), null, 6);
+        insertCard("7_of_spades", context.getString(R.string.description_7_spades), null, 7);
+        insertCard("8_of_spades", context.getString(R.string.description_8_spades), null, 8);
+        insertCard("9_of_spades", context.getString(R.string.description_9_spades), null, 9);
+        insertCard("10_of_spades", context.getString(R.string.description_10_spades), null, 10);
+        insertCard("11_of_spades", context.getString(R.string.description_11_spades), null, 10);
+        insertCard("12_of_spades", context.getString(R.string.description_12_spades), null, 10);
+        insertCard("13_of_spades", context.getString(R.string.description_13_spades), null, 10);
+
     }
 }

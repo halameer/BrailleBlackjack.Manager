5b8e8dc0d4c75ce8d8694573bec7c65adf7c3510
Tu Truong <truongtu@pdx.edu>
2015-03-04 19:00:36 -0800

    Removed previous fragments.  Started from scratch with a new MainActivity and a seperate ViewPager Activity to implementing the swiping for "Learning Braille".

---
+++ mobile/src/main/java/finalproject/ece558/edu/pdx/ece/brailleblackjack/LearnBrailleFragment.java
@@ -0,0 +1,78 @@
+package finalproject.ece558.edu.pdx.ece.brailleblackjack;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+
+/**
+ * A fragment representing a single step in a wizard. The fragment shows a dummy title indicating
+ * the page number, along with some dummy text.
+ */
+public class LearnBrailleFragment extends android.support.v4.app.Fragment {
+    /**
+     * The argument key for the page number this fragment represents.
+     */
+    public static final String ARG_PAGE = "page";
+
+    /**
+     * The fragment's page number, which is set to the argument value for {@link #ARG_PAGE}.
+     */
+    private int mPageNumber;
+
+    /**
+     * Factory method for this fragment class. Constructs a new fragment for the given page number.
+     */
+    public static LearnBrailleFragment create(int pageNumber) {
+        LearnBrailleFragment fragment = new LearnBrailleFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, pageNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public LearnBrailleFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPageNumber = getArguments().getInt(ARG_PAGE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout containing a title and body text.
+        ViewGroup rootView = (ViewGroup) inflater
+                .inflate(R.layout.fragment_learn_braille, container, false);
+
+        ImageView  img = (ImageView)rootView.findViewById(R.id.learnBrailImg);
+
+        switch (mPageNumber+1) {
+            case 1:
+
+                img.setImageDrawable(getResources().getDrawable(R.drawable.numbers));
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+
+        return rootView;
+    }
+
+    /**
+     * Returns the page number represented by this fragment object.
+     */
+    public int getPageNumber() {
+        return mPageNumber;
+    }
+}
--- mobile/src/main/java/finalproject/ece558/edu/pdx/ece/brailleblackjack/MainActivity.java
+++ mobile/src/main/java/finalproject/ece558/edu/pdx/ece/brailleblackjack/MainActivity.java
@@ -1,133 +1,60 @@
 package finalproject.ece558.edu.pdx.ece.brailleblackjack;
 
+
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // On first visit
-        if (savedInstanceState == null) {
-            switchFragment(Tracker.HOME_FRAGMENT);
-        } else {
-
-        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
+public class MainActivity extends ListActivity {
     /**
-     * A placeholder fragment containing a simple view.
+     * This class describes an individual Selection (the Selection title, and the activity class that
+     * demonstrates this Selection).
      */
-    public static class PlaceholderFragment extends Fragment {
+    private class Selection {
+        private CharSequence title;
+        private Class<? extends Activity> activityClass;
 
-        public PlaceholderFragment() {
+        public Selection(int titleResId, Class<? extends Activity> activityClass) {
+            this.activityClass = activityClass;
+            this.title = getResources().getString(titleResId);
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+        public String toString() {
+            return title.toString();
         }
     }
-
 
     /**
-     *  Handles Switching between fragments
-     *
-     *  @param whichFragment - The fragment ID to switch to
+     * The collection of all Selections in the app. This gets instantiated in {@link
+     * #onCreate(android.os.Bundle)} because the {@link Selection} constructor needs access to {@link
+     * android.content.res.Resources}.
      */
-    public void switchFragment(int whichFragment) {
-        // Replace whatever is in the fragment_container view with this fragment
-        Fragment fragment = null;
+    private static Selection[] mSelections;
 
-        switch (whichFragment) {
-            // Open the Home fragment
-            case Tracker.HOME_FRAGMENT:
-                Tracker.curFragment = Tracker.HOME_FRAGMENT;
-                Log.d("MainActivity", "Case Hit Home Switch");
-                fragment = new Home();
-                break;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            // Open the Teaching fragment
-            case Tracker.TEACHING_FRAGMENT:
-                Tracker.curFragment = Tracker.TEACHING_FRAGMENT;
-                Log.d("MainActivity", "Case Hit Teaching Switch");
-                fragment = new Teaching();
-                break;
+        // Instantiate the list of Selections.
+        mSelections = new Selection[]{
+                new Selection(R.string.learn_braille, LearnBrailleActivity.class),
+        };
 
-            // Open the BlackJack fragment
-            case Tracker.BLACKJACK_FRAGMENT:
-                Tracker.curFragment = Tracker.BLACKJACK_FRAGMENT;
-                Log.d("MainActivity", "Case Hit BlackJack Switch");
-                fragment = new BlackJack();
-                break;
+        setListAdapter(new ArrayAdapter<Selection>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                mSelections));
+    }
 
-            // Open the About fragment
-            case Tracker.ABOUT_FRAGMENT:
-                Tracker.curFragment = Tracker.ABOUT_FRAGMENT;
-                Log.d("MainActivity", "Case Hit About Switch");
-                fragment = new About();
-                break;
-
-            // Something wrong happened, reopen Home fragment
-            default: // Switch to Home Screen
-                Tracker.curFragment = Tracker.HOME_FRAGMENT;
-                Log.e("MainActivity", "CASE HIT DEFAULT!!! Opening Start Fragment");
-                fragment = new Home();
-                break;
-        }
-
-
-        // Handle the opening of the new fragment specified in the switch statement
-        if (fragment != null) {
-            Log.d("MainActivity", "Switching Fragments");
-            FragmentTransaction transaction = getFragmentManager()
-                    .beginTransaction();
-
-            transaction.replace(R.id.container, fragment);
-            transaction.addToBackStack(null);
-            transaction.commit();
-        } else {
-            // Error in creating fragment
-            Log.e("MainActivity", "Error in creating fragment");
-        }
+    @Override
+    protected void onListItemClick(ListView listView, View view, int position, long id) {
+        // Launch the Selection associated with this list position.
+        startActivity(new Intent(MainActivity.this, mSelections[position].activityClass));
     }
 }
+

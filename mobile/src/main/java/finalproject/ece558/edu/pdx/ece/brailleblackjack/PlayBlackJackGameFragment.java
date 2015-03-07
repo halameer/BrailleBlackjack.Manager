f04d61d73d77fd4364a6e44a0684410f96bfef81
Tu Truong <truongtu@pdx.edu>
2015-03-06 18:18:50 -0800

    Added initial black jack logic.

--- mobile/src/main/java/finalproject/ece558/edu/pdx/ece/brailleblackjack/PlayBlackJackGameFragment.java
+++ mobile/src/main/java/finalproject/ece558/edu/pdx/ece/brailleblackjack/PlayBlackJackGameFragment.java
@@ -5,6 +5,7 @@ import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,33 +16,35 @@ import java.util.Random;
 
 public class PlayBlackJackGameFragment extends Fragment {
 
-    private ImageView dealer_left_card;
-    private ImageView dealer_right_card;
-    private ImageView player_left_card;
-    private ImageView player_right_card;
+    private Deck curDeck;
 
-    private ImageView dealer_top_total;
-    private ImageView dealer_bot_total;
-    private ImageView player_top_total;
-    private ImageView player_bot_total;
+    private ImageView dealer_left_slot;
+    private ImageView dealer_right_slot;
+    private ImageView player_left_slot;
+    private ImageView player_right_slot;
 
-    private int dealer_left_card_num;
-    private int dealer_right_card_num;
-    private int player_left_card_num;
-    private int player_right_card_num;
+    private int dealer_top_total_value;
+    private int dealer_bot_total_value;
+    private int player_top_total_value;
+    private int player_bot_total_value;
 
-    private int dealer_top_total_num;
-    private int dealer_bot_total_num;
-    private int player_top_total_num;
-    private int player_bot_total_num;
+    private ImageView dealer_top_total_slot;
+    private ImageView dealer_bot_total_slot;
+    private ImageView player_top_total_slot;
+    private ImageView player_bot_total_slot;
+
+    private Card dealer_left_card;
+    private Card dealer_right_card;
+    private Card player_left_card;
+    private Card player_right_card;
+
+    private boolean dealer_had_ace;
+    private boolean player_had_ace;
+    private boolean dealer_stands;
+    private boolean player_stands;
 
     private Button button_hit;
     private Button button_stand;
-
-    public ImageView getRandomCard(View view) {
-        ImageView img = (ImageView) view.findViewById(R.id.learnBrailImg);
-        return img;
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,13 +55,50 @@ public class PlayBlackJackGameFragment extends Fragment {
         // the previous article selection set by onSaveInstanceState().
         // This is primarily necessary when in the two-pane layout.
         if (savedInstanceState != null) {
-            dealer_left_card = getRandomCard(v);
-            dealer_right_card = getRandomCard(v);
-            player_left_card = getRandomCard(v);
-            player_right_card = getRandomCard(v);
 
-            retrieveDealerTotal();
-            retrievePlayerTotal();
+            // Grab the single dealer card on the right and the two player cards
+            curDeck = new Deck(this);
+            dealer_left_card = curDeck.getCard(generateRandomCard());
+            dealer_right_card = curDeck.getCard(generateRandomCard());
+            player_left_card = curDeck.getCard(generateRandomCard());
+            player_right_card = curDeck.getCard(generateRandomCard());
+
+            // Grab initial total(s) for player
+            // Left card IS an Ace, right card is NOT an Ace
+            if (player_left_card.getCardValue() == 1 && player_right_card.getCardValue() > 1){
+                    player_top_total_value = player_left_card.getCardValue()
+                            + player_right_card.getCardValue();
+                    player_bot_total_value = 11
+                            + player_right_card.getCardValue();
+                    player_had_ace = true;
+            }
+            // Left card is NOT an Ace, right card IS an Ace
+            else if (player_right_card.getCardValue() == 1 && player_left_card.getCardValue() > 1){
+                player_top_total_value = player_left_card.getCardValue()
+                        + player_right_card.getCardValue();
+                player_bot_total_value = 11
+                        + player_right_card.getCardValue();
+                player_had_ace = true;
+            }
+            // Both cards are aces
+            else if (player_right_card.getCardValue() == 1 && player_left_card.getCardValue() == 1)
+            {
+                player_top_total_value = 2;
+                player_bot_total_value = 12;
+                player_had_ace = true;
+            }
+            // Both cards are NOT aces
+            else if (player_right_card.getCardValue() > 1 && player_left_card.getCardValue() > 1) {
+                player_top_total_value = player_left_card.getCardValue()
+                        + player_right_card.getCardValue();
+                player_bot_total_value = 0;
+                player_had_ace = false;
+            }
+
+            button_hit.findViewById(R.id.button_hit);
+            button_stand.findViewById(R.id.button_stand);
+
+
         } else {
 
         }
@@ -67,25 +107,84 @@ public class PlayBlackJackGameFragment extends Fragment {
         return v;
     }
 
-    public void retrieveDealerTotal() {
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_hit:
+                playerHits();
+                break;
+            case R.id.button_stand:
+                playerStands();
+                break;
+            default:
+                Log.i("Play Black Jack Game Fragment", "onClick Error: " + view.getId());
+                break;
+        }
     }
-
-
-    public void retrievePlayerTotal() {
-
-    }
-
 
     public void playerHits() {
+        int temp_player_top_total_value;
+        int temp_player_bot_total_value;
 
+        // Grab a new card.
+        player_left_card = player_right_card;
+        player_right_card = curDeck.getCard(generateRandomCard());
+
+        if (player_had_ace){
+            player_top_total_value = player_top_total_value + player_right_card.getCardValue();
+            player_bot_total_value = player_bot_total_value + player_right_card.getCardValue();
+
+            // The Ace being 11 will cause the player to bust.
+            if (player_bot_total_value > 21){
+                player_bot_total_value = 0;
+
+                // Hide the player's bot total
+                player_bot_total_slot.setVisibility(View.INVISIBLE);
+            }
+        }
+        else{
+            player_top_total_value = player_top_total_value + player_right_card.getCardValue();
+
+            if (player_top_total_value > 21){
+                // Player loses.
+                // Pop up a notification.
+            }
+        }
     }
 
     public void playerStands() {
-
+        dealerHits();
     }
 
-    public void getWinner() {
+    public void dealerHits() {
+        int temp_dealer_top_total_value;
+        int temp_dealer_bot_total_value;
+
+        // Grab a new card.
+        dealer_left_card = dealer_right_card;
+        dealer_right_card = curDeck.getCard(generateRandomCard());
+
+        if (dealer_had_ace){
+            dealer_top_total_value = dealer_top_total_value + dealer_right_card.getCardValue();
+            dealer_bot_total_value = dealer_bot_total_value + dealer_right_card.getCardValue();
+
+            // The Ace being 11 will cause the player to bust.
+            if (dealer_bot_total_value > 21){
+                dealer_bot_total_value = 0;
+
+                // Hide the dealer's bot total
+                dealer_bot_total_slot.setVisibility(View.INVISIBLE);
+            }
+        }
+        else{
+            dealer_top_total_value = dealer_top_total_value + dealer_right_card.getCardValue();
+
+            if (dealer_top_total_value > 21){
+                // Player wins.
+                // Pop up a notification.
+            }
+        }
 
     }
 
